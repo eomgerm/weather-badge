85074a7bed9087cce0e3f07c95eadb712dcb4afc
eomgerm <fishbread00@gmail.com>
2022-05-15 11:04:38 +0000

    configurations & web implementations

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: "-apple-system, BlinkMacSystemFont, Apple SD Gothic Neo, Pretendard, Roboto, Noto Sans KR, Segoe UI, Malgun Gothic, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, sans-serif",
+    },
+    screens: {
+      xs: "20rem",
+      xl: "576px",
+    },
+    container: {
+      center: true,
+    },
+  },
+  plugins: [],
+};
