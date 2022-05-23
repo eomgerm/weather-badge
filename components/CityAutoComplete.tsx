5a3a5af61dfbec3d568fa3a9cad29dd7b621cfcb
eomgerm <fishbread00@gmail.com>
2022-05-23 07:27:00 +0000

    debouncing

--- components/CityAutoComplete.tsx
+++ components/CityAutoComplete.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import CityList from "./CityList";
 import NotFoundCity from "./NotFoundCity";
 import BadInput from "./BadInput";
+import useDebounce from "../hooks/useDebounce";
 
 type Props = {
   input: string;
@@ -14,24 +15,33 @@ type Props = {
 const CityAutoComplete: NextPage<Props> = ({ input, setChosenCity }: Props) => {
   const [recommendations, setRecommendations] = useState<Array<City>>([]);
 
+  const debouncedInput = useDebounce({ value: input, delay: 300 });
+
   useEffect(() => {
     const getCityRecommendations = async () => {
-      const { data } = await axios(`/api/geo?city=${input}`);
+      const { data } = await axios(`/api/geo?city=${debouncedInput}}`);
       const cities = data.map((city: any) => ({ name: city.name, country: city.country, lat: city.lat, lon: city.lon }));
       setRecommendations(cities);
     };
-    if (input) {
+
+    if (debouncedInput) {
       getCityRecommendations();
+    } else {
+      setRecommendations([]);
     }
-  }, [input]);
+
+    return () => {
+      setRecommendations([]);
+    };
+  }, [debouncedInput]);
 
   return (
     <div className="bg-white rounded-md flex flex-col w-[20rem] h-[15.25rem] justify-center items-center overflow-y-auto">
-      {input ? (
+      {debouncedInput ? (
         recommendations.length > 0 ? (
           <CityList setChosenCity={setChosenCity} recommendations={recommendations} />
         ) : (
-          <NotFoundCity input={input} />
+          <NotFoundCity input={debouncedInput} />
         )
       ) : (
         <BadInput />
