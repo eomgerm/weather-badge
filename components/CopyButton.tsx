398e62eb7291688b12e34b432070c5de1f9fd916
eomgerm <fishbread00@gmail.com>
2022-05-23 03:56:59 +0000

    버튼 클릭했을 때 생기는 outine 제거

--- components/CopyButton.tsx
+++ components/CopyButton.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 
 const CopyButton: NextPage = () => {
   return (
-    <button className="mb-5 bg-gradient-to-tr from-[#f650a0] to-[#ff8787] rounded-full py-3 w-56 text-[#FFE4F6] font-bold hover:bg-none hover:bg-[#ff8787]">
+    <button className="mb-5 bg-gradient-to-tr from-[#f650a0] to-[#ff8787] rounded-full py-3 w-56 text-[#FFE4F6] font-bold hover:bg-none hover:bg-[#ff8787] focus:outline-none">
       Copy!
     </button>
   );
