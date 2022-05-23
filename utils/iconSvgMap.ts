42c3d615fc2b6bb2a2302a2d5d17ed50f1eb0786
eomgerm <fishbread00@gmail.com>
2022-05-23 07:29:10 +0000

    OpenWeatherMap API의 아이콘 코드와 SVG 매핑

---
+++ utils/iconSvgMap.ts
@@ -0,0 +1,21 @@
+type IconSVGMap = {
+  [key: string]: string;
+};
+
+const IconSVGMap: IconSVGMap = {
+  "11d": "/animated/thunder.svg",
+  "09d": "/animated/rainy-6.svg",
+  "10d": "/animated/rainy-3.svg",
+  "13d": "/animated/snowy-5.svg",
+  "50d": "/animated/cloudy.svg",
+  "01d": "/animated/day.svg",
+  "01n": "/animated/night.svg",
+  "02d": "/animated/cloudy-day-1.svg",
+  "02n": "/animated/cloudy-night-1.svg",
+  "03d": "/animated/cloudy-day-2.svg",
+  "03n": "/animated/cloudy-day-3.svg",
+  "04d": "/animated/cloudy-day-3.svg",
+  "04n": "/animated/cloudy-night-3.svg",
+};
+
+export default IconSVGMap;
