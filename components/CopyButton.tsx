aa02c6fef071ec6427fbe9d8c026bdef13cef9fd
eomgerm <fishbread00@gmail.com>
2022-05-29 10:34:03 +0000

    daisyUI를 활용한 modal button으로 수정

--- components/CopyButton.tsx
+++ components/CopyButton.tsx
@@ -1,10 +1,18 @@
 import { NextPage } from "next";
 
-const CopyButton: NextPage = () => {
+type CopyButtonProps = {
+  onClick: () => void;
+};
+
+const CopyButton: NextPage<CopyButtonProps> = ({ onClick }: CopyButtonProps) => {
   return (
-    <button className="mb-5 bg-gradient-to-tr from-[#f650a0] to-[#ff8787] rounded-full py-3 w-56 text-[#FFE4F6] font-bold hover:bg-none hover:bg-[#ff8787] focus:outline-none">
+    <label
+      onClick={onClick}
+      className="text-center cursor-pointer mb-5 bg-gradient-to-tr from-[#f650a0] to-[#ff8787] rounded-full py-3 w-56 text-[#FFE4F6] font-bold hover:bg-none hover:bg-[#ff8787] focus:outline-none focus:bg-[#ff8787]"
+      htmlFor="copy-modal"
+    >
       Copy!
-    </button>
+    </label>
   );
 };
 
