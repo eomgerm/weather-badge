ae39b9419c3322dc64869faf0cb416ff7341adf5
eomgerm <fishbread00@gmail.com>
2022-05-29 10:36:56 +0000

    props 삭제

--- components/CopyButton.tsx
+++ components/CopyButton.tsx
@@ -1,13 +1,8 @@
 import { NextPage } from "next";
 
-type CopyButtonProps = {
-  onClick: () => void;
-};
-
-const CopyButton: NextPage<CopyButtonProps> = ({ onClick }: CopyButtonProps) => {
+const CopyButton: NextPage = () => {
   return (
     <label
-      onClick={onClick}
       className="text-center cursor-pointer mb-5 bg-gradient-to-tr from-[#f650a0] to-[#ff8787] rounded-full py-3 w-56 text-[#FFE4F6] font-bold hover:bg-none hover:bg-[#ff8787] focus:outline-none focus:bg-[#ff8787]"
       htmlFor="copy-modal"
     >
