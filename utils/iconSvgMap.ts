fe0da2858d1f9d9500e0e9689ec9708f234d713c
eomgerm <fishbread00@gmail.com>
2022-05-29 09:37:07 +0000

    image별 scale, y 위치 추가

--- utils/iconSvgMap.ts
+++ utils/iconSvgMap.ts
@@ -1,21 +1,21 @@
 type IconSVGMap = {
-  [key: string]: string;
+  [key: string]: { uri: string; scale: number; y: number };
 };
 
 const IconSVGMap: IconSVGMap = {
-  "11d": "/animated/thunder.svg",
-  "09d": "/animated/rainy-6.svg",
-  "10d": "/animated/rainy-3.svg",
-  "13d": "/animated/snowy-5.svg",
-  "50d": "/animated/cloudy.svg",
-  "01d": "/animated/day.svg",
-  "01n": "/animated/night.svg",
-  "02d": "/animated/cloudy-day-1.svg",
-  "02n": "/animated/cloudy-night-1.svg",
-  "03d": "/animated/cloudy-day-2.svg",
-  "03n": "/animated/cloudy-day-3.svg",
-  "04d": "/animated/cloudy-day-3.svg",
-  "04n": "/animated/cloudy-night-3.svg",
+  "11d": { uri: "/animated/thunder.svg", scale: 0.8, y: 5 },
+  "09d": { uri: "/animated/rainy-6.svg", scale: 0.8, y: 0 },
+  "10d": { uri: "/animated/rainy-3.svg", scale: 0.8, y: 5 },
+  "13d": { uri: "/animated/snowy-5.svg", scale: 0.8, y: 0 },
+  "50d": { uri: "/animated/cloudy.svg", scale: 0.9, y: 5 },
+  "01d": { uri: "/animated/day.svg", scale: 1.0, y: -10 },
+  "01n": { uri: "/animated/night.svg", scale: 1.0, y: -10 },
+  "02d": { uri: "/animated/cloudy-day-1.svg", scale: 0.9, y: 5 },
+  "02n": { uri: "/animated/cloudy-night-1.svg", scale: 0.9, y: 5 },
+  "03d": { uri: "/animated/cloudy-day-2.svg", scale: 0.9, y: 5 },
+  "03n": { uri: "/animated/cloudy-night-2.svg", scale: 0.9, y: 5 },
+  "04d": { uri: "/animated/cloudy-day-3.svg", scale: 0.9, y: 5 },
+  "04n": { uri: "/animated/cloudy-night-3.svg", scale: 0.9, y: 5 },
 };
 
 export default IconSVGMap;
