fb4d4937c695e54b285f1fc13bc3f717dc57c3c9
eomgerm <fishbread00@gmail.com>
2022-05-29 10:33:45 +0000

    사이즈 조절 inupt disabled...

--- components/SizeInput.tsx
+++ components/SizeInput.tsx
@@ -9,15 +9,16 @@ type SizeInputProps = {
 const SizeInput: NextPage<SizeInputProps> = ({ onChange }: SizeInputProps) => {
   return (
     <>
-      <label className="label">Size</label>
+      <label className="label">Size: Coming Soon...</label>
       <div className="relative">
         <FontAwesomeIcon icon={faMaximize} size="lg" className="absolute top-3.5 left-4" />
         <input
           type="number"
-          placeholder="100 (Default)"
+          placeholder="150 (Default)"
           className="py-3 pl-11 pr-5 rounded-full w-full focus:outline-0 focus:ring-4 focus: ring-[#A499C6]/50"
           name="size"
           onChange={onChange}
+          disabled
         />
       </div>
     </>
