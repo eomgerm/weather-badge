234f344512a133495ee9664d67a316ba59c94a65
eomgerm <fishbread00@gmail.com>
2022-05-31 13:14:02 +0000

    [FIX] img 태그 next Image로 변경, rel 속성 추가

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
 
 const Footer: NextPage = () => {
   return (
@@ -9,17 +10,17 @@ const Footer: NextPage = () => {
       <div className="flex justify-center items-center container">
         <div>
           © {new Date().getFullYear()}{" "}
-          <a href="https://github.com/eomgerm" target="_blank" className="hover:underline">
+          <a href="https://github.com/eomgerm" target="_blank" rel="noopener" className="hover:underline">
             Eomgerm
           </a>
         </div>
         <div className="flex gap-x-4 items-center ml-auto">
-          <a href="https://github.com/eomgerm/weather-badge" target="_blank">
+          <a href="https://github.com/eomgerm/weather-badge" target="_blank" rel="noopener">
             {/*@ts-ignore*/}
             <FontAwesomeIcon icon={faGithub} size="xl" style={{ color: "005B3A" }} />
           </a>
-          <a href="https://velog.io/@eomgerm" target="_blank">
-            <img src="/velog.svg" width={25} />
+          <a href="https://velog.io/@eomgerm" target="_blank" rel="noopener" className="h-[25px]">
+            <Image src="/velog.svg" width={25} height={25} alt="velog" />
           </a>
           <a href="mailto:fishbread00@gmail.com">
             {/*@ts-ignore*/}
